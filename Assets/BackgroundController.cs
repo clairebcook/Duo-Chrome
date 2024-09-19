c1645102d25f0ce61a2cf8f858e95a635e1871ac
Olivia Feldman <>
2024-09-19 16:45:41 +0200

    created background controller

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public Camera cam;
+    public Color color1;
+    public Color color2;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
