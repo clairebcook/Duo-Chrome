74b8b09562a4ebe57d9d5f988f1698c7252e6f7f
Dylan O’Connor <dylanoconnor@Dylans-MacBook-Pro.local>
2024-09-23 08:40:19 -0700

    death script

---
+++ Assets/Scripts/MakeDeadly.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+
+public class OnDeath : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other) {
+        var player = other.collider.GetComponent<Player1>();
+
+        if (player != null) {
+            player.Die();
+        }
+
+    }
+}
