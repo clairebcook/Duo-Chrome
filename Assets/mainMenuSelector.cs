48a0789187e25466154f4fdccb4500219feaa9ef
claire <ccpinkb@icloud.com>
2024-09-30 16:27:28 +0200

    no message

--- Assets/mainMenuSelector.cs
+++ Assets/mainMenuSelector.cs
@@ -3,11 +3,30 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    public GameObject startButton;
-
     public void PlayGame()
     {
         SceneManager.LoadScene("tutorialLevel");
+
+    public void LevelSelector()
+     {
+            SceneManager.LoadScene("levelSelect");
+     }
+
+        public void OpenGameControls()
+        {
+            // Here, you can either load another scene, or show a panel with the controls.
+            // Example: SceneManager.LoadScene("GameControlsScene");
+        }
+
+        public void OpenSettings()
+        {
+            // Same as Game Controls, either load a scene or show a settings panel.
+            // Example: SceneManager.LoadScene("SettingsScene");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit(); // Quits the game (only works in the built version, not in the Unity editor).
+        }
     }
 }
-
