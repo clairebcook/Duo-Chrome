9335c5a560c7fd1cc51e9ea3707f6178d879d1be
Dylan O’Connor <dylanoconnor@Dylans-MacBook-Pro.local>
2024-10-06 10:09:16 -0700

    easter egg

---
+++ Assets/Scripts/EasterEgg.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+public class EasterEgg : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float collectForce = 5;
+
+    public AudioSource audioS;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+
+    }
+
+    // called on the easter egg when the player collides with it
+    private IEnumerator CollectMe() {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, collectForce);
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+
+    // audio coroutine
+    private IEnumerator PlayJingle() {
+        if (audioS != null) {
+            audioS.Play();
+        }
+        yield return new WaitForSeconds(1);
+    }
+
+    // collect the item when walked on
+    private void OnTriggerEnter2D(Collider2D other) {
+        var player = other.GetComponent<Player1>();
+
+        if (player != null) {
+            StartCoroutine(PlayJingle());
+            StartCoroutine(CollectMe());
+        }
+
+    }
+    
+}
